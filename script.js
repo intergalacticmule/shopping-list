ede357d3cb1c96acc48c63c796dca3b800ffacb9
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-30 18:50:11 +0200

    Add textbox event listener for enter keypress

--- script.js
+++ script.js
@@ -4,46 +4,52 @@ const unorderedList = document.querySelector("ul");
 let listButtons;
 
 function removeParent(element) {
-    element.parentElement.remove();
+	element.parentElement.remove();
 }
 
 function addElementsToParent(parent) {
-    li = document.createElement("li");
-    li.textContent = textBox.value;
-    button = document.createElement("button");
-    button.textContent = "Delete";
-    button.classList.add("delete-button");
+	li = document.createElement("li");
+	li.textContent = textBox.value;
+	button = document.createElement("button");
+	button.textContent = "Delete";
+	button.classList.add("delete-button");
 
-    li.appendChild(button);
-    parent.appendChild(li);
+	li.appendChild(button);
+	parent.appendChild(li);
 
-    listButtons = document.querySelectorAll(".delete-button");
+	listButtons = document.querySelectorAll(".delete-button");
 }
 
 function addListButtonsFunctionality(buttons) {
-    buttons.forEach((button) => {
-        button.addEventListener("click", () => {
-            removeParent(button);
-        });
-    });
+	buttons.forEach((button) => {
+		button.addEventListener("click", () => {
+			removeParent(button);
+		});
+	});
 }
 
 function clearInputField(field) {
-    field.value = "";
+	field.value = "";
 }
 
 function checkUserInput(field) {
-    return field.value == "" ? false : true;
+	return field.value == "" ? false : true;
 }
 
 addButton.addEventListener("click", () => {
-    if (checkUserInput(textBox)) {
-        addElementsToParent(unorderedList);
-        addListButtonsFunctionality(listButtons);
-        clearInputField(textBox);
-        textBox.focus();
-    } else {
-        textBox.focus();
-        return;
-    }
+	if (checkUserInput(textBox)) {
+		addElementsToParent(unorderedList);
+		addListButtonsFunctionality(listButtons);
+		clearInputField(textBox);
+		textBox.focus();
+	} else {
+		textBox.focus();
+		return;
+	}
+});
+
+textBox.addEventListener("keypress", (e) => {
+	if (e.key == "Enter") {
+		addButton.click();
+	}
 });
