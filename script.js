187ead6af8d4afba5f69b0d81ce6366d5f3f11bd
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-30 17:37:48 +0200

    Add functionalities to addButton EventListener Add list element delete button functionality Add clear input field functionality Add focus to input field

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ const textBox = document.querySelector(".container-textbox > input");
 const unorderedList = document.querySelector("ul");
 let listButtons;
 
+function removeParent(element) {
+  element.parentElement.remove();
+}
+
+function addListButtonsFunctionality(buttons) {
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      removeParent(button);
+    });
+  });
+}
+
+function clearInputField(field) {
+  field.value = "";
+}
+
+function checkUserInput(field) {
+  return field.value == "" ? true : false;
+}
+
 addButton.addEventListener("click", () => {
   div = document.createElement("div");
   li = document.createElement("li");
@@ -17,6 +37,9 @@ addButton.addEventListener("click", () => {
   container.appendChild(div);
 
   listButtons = document.querySelectorAll(".delete-button");
+  addListButtonsFunctionality(listButtons);
+  clearInputField(textBox);
+  textBox.focus();
 });
 
 // Add event listener to addButton that adds a div with list element and a button child to unorderedList;
