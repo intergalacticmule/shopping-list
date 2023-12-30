38baca80cd8b05440990c9907d8fc6e3928622df
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-30 16:38:31 +0200

    Add addButton logic to script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+container = document.querySelector(".container-textbox");
 addButton = document.querySelector(".container-textbox > button");
+textBox = document.querySelector(".container-textbox > input");
 unorderedList = document.querySelector("ul");
 
-{
-  /* <li>test<button>test</button></li> */
-}
+addButton.addEventListener("click", () => {
+  div = document.createElement("div");
+  li = document.createElement("li");
+  button = document.createElement("button");
+  li.textContent = textBox.value;
+  button.textContent = "Delete";
 
-// Add event listener to addButton that adds a list element with a button child to unorderedList;
+  div.appendChild(li);
+  div.appendChild(button);
+  container.appendChild(div);
+});
+
+/* <div>
+        <li></li>
+        <button></button>
+    </div> */
+
+// Add event listener to addButton that adds a div with list element and a button child to unorderedList;
 
 // Add event listeners to all delete buttons of list elements using for each that deletes their parent element;
