2051bde9f0e45bbb06ff2d3512a6bcd636061098
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-30 16:23:05 +0200

    Add selectors and pseudocode to script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+addButton = document.querySelector(".container-textbox > button");
+unorderedList = document.querySelector("ul");
+
+{
+  /* <li>test<button>test</button></li> */
+}
+
+// Add event listener to addButton that adds a list element with a button child to unorderedList;
+
+// Add event listeners to all delete buttons of list elements using for each that deletes their parent element;
