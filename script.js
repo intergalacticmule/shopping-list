383af0e48aa59f96225c1813967e4523553e5b34
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-30 18:33:54 +0200

    Refactor script.js, add margin to list buttons

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
-const container = document.querySelector(".container-textbox");
 const addButton = document.querySelector(".container-textbox > button");
 const textBox = document.querySelector(".container-textbox > input");
 const unorderedList = document.querySelector("ul");
 let listButtons;
 
 function removeParent(element) {
-  element.parentElement.remove();
+    element.parentElement.remove();
+}
+
+function addElementsToParent(parent) {
+    li = document.createElement("li");
+    li.textContent = textBox.value;
+    button = document.createElement("button");
+    button.textContent = "Delete";
+    button.classList.add("delete-button");
+
+    li.appendChild(button);
+    parent.appendChild(li);
+
+    listButtons = document.querySelectorAll(".delete-button");
 }
 
 function addListButtonsFunctionality(buttons) {
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      removeParent(button);
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            removeParent(button);
+        });
     });
-  });
 }
 
 function clearInputField(field) {
-  field.value = "";
+    field.value = "";
 }
 
 function checkUserInput(field) {
-  return field.value == "" ? true : false;
+    return field.value == "" ? false : true;
 }
 
 addButton.addEventListener("click", () => {
-  li = document.createElement("li");
-  li.textContent = textBox.value;
-  button = document.createElement("button");
-  button.textContent = "Delete";
-  button.classList.add("delete-button");
-
-  li.appendChild(button);
-  unorderedList.appendChild(li);
-
-  listButtons = document.querySelectorAll(".delete-button");
-  addListButtonsFunctionality(listButtons);
-  clearInputField(textBox);
-  textBox.focus();
+    if (checkUserInput(textBox)) {
+        addElementsToParent(unorderedList);
+        addListButtonsFunctionality(listButtons);
+        clearInputField(textBox);
+        textBox.focus();
+    } else {
+        return;
+    }
 });
-
-// Add event listener to addButton that adds a div with list element and a button child to unorderedList;
-
-// Add event listeners to all delete buttons of list elements using for each that deletes their parent element;
