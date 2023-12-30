6d70596e6bbdb3c22539b6717469b536f7ee823b
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-30 18:10:35 +0200

    Revert "Add functionalities to addButton EventListener"
    This reverts commit 187ead6af8d4afba5f69b0d81ce6366d5f3f11bd.

--- script.js
+++ script.js
@@ -4,26 +4,6 @@ const textBox = document.querySelector(".container-textbox > input");
 const unorderedList = document.querySelector("ul");
 let listButtons;
 
-function removeParent(element) {
-  element.parentElement.remove();
-}
-
-function addListButtonsFunctionality(buttons) {
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      removeParent(button);
-    });
-  });
-}
-
-function clearInputField(field) {
-  field.value = "";
-}
-
-function checkUserInput(field) {
-  return field.value == "" ? true : false;
-}
-
 addButton.addEventListener("click", () => {
   li = document.createElement("li");
   li.textContent = textBox.value;
@@ -35,9 +15,6 @@ addButton.addEventListener("click", () => {
   unorderedList.appendChild(li);
 
   listButtons = document.querySelectorAll(".delete-button");
-  addListButtonsFunctionality(listButtons);
-  clearInputField(textBox);
-  textBox.focus();
 });
 
 // Add event listener to addButton that adds a div with list element and a button child to unorderedList;
